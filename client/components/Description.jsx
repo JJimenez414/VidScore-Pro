5373702cd97c88a9921db77c041da02c4f6ed031
Jose <joseangel130414@gmail.com>
2024-06-09 22:16:48 -0500

    Jose - Began sketching the basic layout of the website.

---
+++ client/components/Description.jsx
@@ -0,0 +1,9 @@
+
+
+function Description() {
+    return ( 
+      <h1>This is description</h1>
+    )
+}
+
+export default Description;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,29 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from "axios"
+import Dropdown from "../components/Dropdown"
+import Grade from "../components/Grade"
+import Description from "../components/Description"
+import Feedback from "../components/Feedback"
+import FileDrop from "../components/FileDrop"
+import Header from "../components/Header"
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
-
-  const fetchAPI = async() => {
-    const response = await axios.get("http://127.0.0.1:8080/api/users")
-    console.log(response.data.users)
-  }
-
-  useEffect( () => {
-    fetchAPI();
-  }, []);
-
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="grid"> 
+      
+        <h1> Hello world</h1>
+
+        <Dropdown />
+        <Description />
+        <Grade />
+        <Feedback />
+        <FileDrop />
+      
       </div>
-      <h1>Jose and Carlos</h1>
-      <h1>Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Hellsdfo world
-      </p>
     </>
   )
 }
