df005ec92702b25e7c7f1ccb07a5e7b16f4997f6
bravodude <85579815+bravodude@users.noreply.github.com>
2024-06-11 16:05:42 -0500

    change output on backend side

--- server/main.py
+++ server/main.py
@@ -1,21 +1,10 @@
-from flask import Flask, jsonify
-# from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-# cors = CORS(app, origins="*")
 
-@app.route('/api/users', methods=['GET'])
-def uers():
-    return jsonify(
-        {
-            "users": [
-                'Jose',
-                'Carlos',
-                'Mike'
-            ]
-        }
-    )
+@app.route('/')
+def index():
+    return "Hello world"
 
 if __name__ == '__main__':
     app.run(debug=True, port="8080")
-    
