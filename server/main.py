c2cc23353b26a27f9ac5466a55003d00ceb6d811
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2024-06-12 14:07:08 -0500

    add video resolution function

--- server/main.py
+++ server/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from moviepy.editor import VideoFileClip
+import cv2
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return video_length()
+    return video_resolution()
 
 
 # Function will locate video file and get duration of clip, and test to see if the duration meets the ideal length. Returns string
@@ -23,6 +25,18 @@ def video_length():
         return f"The video is too long. {int(seconds)} seconds is {int(seconds - ideal_length)} seconds much longer than the ideal range"
     else:
         return f"The video is a good length. At {int(seconds)} seconds, the video is {int(ideal_length - seconds)} seconds below ideal range."
+
+def video_resolution():
+
+    path = './videos/video1.mp4'
+    vid = cv2.VideoCapture(path)
+
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    output = f"The resolution is {int(height)} x {int(width)} pixels."
+
+    return output
 
 
 
