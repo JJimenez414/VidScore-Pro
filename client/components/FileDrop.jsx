da2e21648ba0412b7cb51dff8e6c0108c6c6bbcc
Jose <joseangel130414@gmail.com>
2024-06-12 14:22:05 -0500

    Jose - Was ablel to get the file uplaaded from the drop box.

--- client/components/FileDrop.jsx
+++ client/components/FileDrop.jsx
@@ -1,9 +1,25 @@
 import logoDrop from "/src/assets/dropbox.svg"
+import { useRef } from "react";
 
 function FileDrop () {
+
+    const getFile = useRef(null);
+
+
+    function handleChange(event) {
+        event.preventDefault();
+        console.log(getFile.current.files[0].name);
+    }
+
     return (
-        <div className="fileDropBox">
-            <img src={logoDrop} alt="" className="dropImg" />
+        <div className="dropBoxContainer">
+            <label htmlFor="input-video" id="inputLabel"> 
+                <input ref={getFile} type="file" id="input-video" hidden onChange={handleChange}/>
+                <div className="dropBoxFile">
+                    <img src={logoDrop} alt="" className="boxImg"/>
+                    <p>File Drop</p>
+                </div>
+            </label>
         </div>
     )
 }
